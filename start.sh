b9b3b47cceaff6c231a0610591aa3ca1aed47abc
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-12-05 06:39:18 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/ZauteKm/UsePyrogramBot.git /UsePyrogramBot
+git clone https://github.com/yasirarism/UsePyrogramBot.git /UsePyrogramBot
 cd /UsePyrogramBot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
